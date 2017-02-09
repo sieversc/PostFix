9ab12afc2c5839123af8d2ac17785fd624747545
sieversc <sieversc@xavier.edu>
2017-02-08 20:02:29 -0500

    String problems some resolved. not all

--- Interface.C
+++ Interface.C
@@ -1,33 +1,42 @@
 /*------------------------------------------------------------------
-A test driver for the stack/bucket class.
 
-Written by Mikeyg
 ------------------------------------------------------------------*/
 
 #include <iostream>
+#include <string>
 #include "LLStacks.h"
 #include "LLQueue.h"
+#include <cstdlib>
 
 using namespace std;
 
+string GetInputString();
+
 int main () {
   // Declare some useful variables
-  int response;
   int temp;
+  int response =1;
 
   // Instantiate/construct a bucket object
-  Stack slop;
-  LLQueue queue;
+  Stack stack;
+  Queue queue;
   
   // Get the user's intention
   cout << "Enter Post-Fix equation to evaluate: ";
-  cin >> response;
-
+  string inputString = GetInputString();
+ 
   queue.Enqueue(response);
+  stack.Push(response);
 
+  stack.Pop(temp);
   queue.Dequeue(temp);
 
   cout << temp;
-  return(0);
 }
 
+string GetInputString(){
+  string response;
+  cout << "\n> ";
+  getline(cin, response);
+  return response;
+}
